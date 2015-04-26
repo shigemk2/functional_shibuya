77e4446b6768087739702d7e5645a41c60d567c0
shigemk2 <i.am.shige@gmail.com>
2015-04-26 16:10:38 +0900

    3-16

---
+++ chapter03/16.answer.scala
@@ -0,0 +1,26 @@
+sealed trait List[+A]
+case object Nil extends List[Nothing]
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def apply[A](as: A*): List[A] = // Variadic function syntax
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+}
+
+def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+  as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+def add1(l: List[Int]): List[Int] =
+  foldRight(l, Nil:List[Int])((h,t) => Cons(h+1,t))
+
+
+val a = Cons(1,Cons(2,Cons(3,Nil)))
+val b = List(1,2,3,4)
+println(a)
+println(b)
+println(add1(a))
+println(add1(List(3,4,5)))
