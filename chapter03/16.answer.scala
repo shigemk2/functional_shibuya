f02072a4b79ba29718750096eb7b672196a6501e
shigemk2 <i.am.shige@gmail.com>
2015-04-26 16:29:19 +0900

    foldRightも突っ込む

--- chapter03/16.answer.scala
+++ chapter03/16.answer.scala
@@ -1,13 +1,7 @@
 import datastructures._
 
-def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
-  as match {
-    case Nil => z
-    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
-  }
-
 def add1(l: List[Int]): List[Int] =
-  foldRight(l, Nil:List[Int])((h,t) => Cons(h+1,t))
+  List.foldRight(l, Nil:List[Int])((h,t) => Cons(h+1,t))
 
 
 val a = Cons(1,Cons(2,Cons(3,Nil)))
--- chapter03/List.scala
+++ chapter03/List.scala
@@ -7,5 +7,11 @@ package datastructures {
     def apply[A](as: A*): List[A] = // Variadic function syntax
       if (as.isEmpty) Nil
       else Cons(as.head, apply(as.tail: _*))
+
+    def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
+      as match {
+        case Nil => z
+        case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+      }
   }
 }
