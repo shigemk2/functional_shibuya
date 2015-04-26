9c2324c67647baa9fe77e0b86a90872a02319112
shigemk2 <i.am.shige@gmail.com>
2015-04-26 16:24:07 +0900

    Listのコンパニオンオブジェクトをpackageに分離

--- chapter03/16.answer.scala
+++ chapter03/16.answer.scala
@@ -1,12 +1,4 @@
-sealed trait List[+A]
-case object Nil extends List[Nothing]
-case class Cons[+A](head: A, tail: List[A]) extends List[A]
-
-object List {
-  def apply[A](as: A*): List[A] = // Variadic function syntax
-    if (as.isEmpty) Nil
-    else Cons(as.head, apply(as.tail: _*))
-}
+import datastructures._
 
 def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
   as match {
---
+++ chapter03/List.scala
@@ -0,0 +1,11 @@
+package datastructures {
+  sealed trait List[+A]
+  case object Nil extends List[Nothing]
+  case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+  object List {
+    def apply[A](as: A*): List[A] = // Variadic function syntax
+      if (as.isEmpty) Nil
+      else Cons(as.head, apply(as.tail: _*))
+  }
+}
