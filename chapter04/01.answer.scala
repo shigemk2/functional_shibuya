60e3a7d05111c5a57ee78f46d8609fb16564bd25
shigemk2 <i.am.shige@gmail.com>
2015-05-21 21:44:48 +0900

    4-1 getOrElse

--- chapter04/01.answer.scala
+++ chapter04/01.answer.scala
@@ -2,3 +2,7 @@ import errorhandling._
 
 println(Some(1))
 println(Some(1).map((x: Int) => x + 1))
+
+println(Some(1).getOrElse(1))
+val v = List(Some("A"), None)
+for(each <- v) println(each.getOrElse("empty"))
--- chapter04/Option.scala
+++ chapter04/Option.scala
@@ -6,6 +6,11 @@ package errorhandling {
       case None => None
       case Some(a) => Some(f(a))
     }
+
+    def getOrElse[B>:A](default: => B): B = this match {
+      case None => default
+      case Some(a) => a
+    }
   }
   case class Some[+A](get: A) extends Option[A]
   case object None extends Option[Nothing]
