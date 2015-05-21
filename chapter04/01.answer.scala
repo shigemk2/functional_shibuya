51eb70298897a8299a745b38d6ab68c61c087969
shigemk2 <i.am.shige@gmail.com>
2015-05-21 21:09:50 +0900

    4-1

---
+++ chapter04/01.answer.scala
@@ -0,0 +1,4 @@
+import errorhandling._
+
+println(Some(1))
+println(Some(1).map((x: Int) => x + 1))
---
+++ chapter04/Option.scala
@@ -0,0 +1,12 @@
+package errorhandling {
+  import scala.{Option => _, Either => _, _}
+
+  sealed trait Option[+A] {
+    def map[B](f: A => B): Option[B] = this match {
+      case None => None
+      case Some(a) => Some(f(a))
+    }
+  }
+  case class Some[+A](get: A) extends Option[A]
+  case object None extends Option[Nothing]
+}
