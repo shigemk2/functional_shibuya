f7f79bb91f11d58656cc7e119e5f609f5f7a5c4e
shigemk2 <i.am.shige@gmail.com>
2015-06-11 14:21:49 +0900

    5-1 コードを突っ込む

--- chapter05/01.answer.scala
+++ chapter05/01.answer.scala
@@ -1,3 +1,42 @@
 import Stream._
+trait Stream[+A] {
+  def toListRecursive: List[A] = this match {
+    case Cons(h,t) => h() :: t().toListRecursive
+    case _ => List()
+  }
 
-println(Stream(1,2))
+  def toList: List[A] = {
+    @annotation.tailrec
+    def go(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Cons(h,t) => go(t(), h() :: acc)
+      case _ => acc
+    }
+    go(this, List()).reverse
+  }
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+
+  val ones: Stream[Int] = Stream.cons(1, ones)
+
+}
+
+object Answer01 {
+  import Stream._
+  def main(args: Array[String]) = {
+    println(Stream(1,2))
+  }
+}
--- chapter05/Stream.scala
+++ chapter05/Stream.scala
@@ -1,18 +1,18 @@
 import Stream._
 trait Stream[+A] {
-  // def toListRecursive: List[A] = this match {
-  //   case Cons(h,t) => h() :: t().toListRecursive
-  //   case _ => List()
-  // }
+  def toListRecursive: List[A] = this match {
+    case Cons(h,t) => h() :: t().toListRecursive
+    case _ => List()
+  }
 
-  // def toList: List[A] = {
-  //   @annotation.tailrec
-  //   def go(s: Stream[A], acc: List[A]): List[A] = s match {
-  //     case Cons(h,t) => go(t(), h() :: acc)
-  //     case _ => acc
-  //   }
-  //   go(this, List()).reverse
-  // }
+  def toList: List[A] = {
+    @annotation.tailrec
+    def go(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Cons(h,t) => go(t(), h() :: acc)
+      case _ => acc
+    }
+    go(this, List()).reverse
+  }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -31,4 +31,12 @@ object Stream {
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
+
+}
+
+object Answer01 {
+  import Stream._
+  def main(args: Array[String]) = {
+    println(Stream(1,2))
+  }
 }
