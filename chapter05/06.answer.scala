49f913fa59efdf4e6e3f605ec5e74fc9e7d35921
shigemk2 <i.am.shige@gmail.com>
2015-06-13 17:57:34 +0900

    5-6 headOption

---
+++ chapter05/06.answer.scala
@@ -0,0 +1,49 @@
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+
+  val ones: Stream[Int] = Stream.cons(1, ones)
+
+}
+
+import Stream._
+trait Stream[+A] {
+  def toList: List[A] = {
+    @annotation.tailrec
+    def go(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Cons(h,t) => go(t(), h() :: acc)
+      case _ => acc
+    }
+    go(this, List()).reverse
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case Cons(h,t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+  // 06
+  def headOption: Option[A] =
+    foldRight(None: Option[A])((h,_) => Some(h))
+}
+
+object Answer06 {
+  import Stream._
+  def main(args: Array[String]) = {
+    println(Stream(1,2,3,4,5).headOption)
+    println(Stream(5,4,3,2,1).headOption)
+  }
+}
