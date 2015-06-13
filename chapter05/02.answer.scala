62d15d0e960c1c7612006d564fdb72b7d6f1b696
shigemk2 <i.am.shige@gmail.com>
2015-06-13 15:46:59 +0900

    5-2

---
+++ chapter05/02.answer.scala
@@ -0,0 +1,62 @@
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+
+  val ones: Stream[Int] = Stream.cons(1, ones)
+
+}
+
+import Stream._
+trait Stream[+A] {
+  // 01
+  def toListRecursive: List[A] = this match {
+    case Cons(h,t) => h() :: t().toListRecursive
+    case _ => List()
+  }
+
+  def toList: List[A] = {
+    @annotation.tailrec
+    def go(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Cons(h,t) => go(t(), h() :: acc)
+      case _ => acc
+    }
+    go(this, List()).reverse
+  }
+
+  // 02
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
+
+  @annotation.tailrec
+  final def drop(n: Int): Stream[A] = this match {
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
+
+}
+
+object Answer02 {
+  import Stream._
+  def main(args: Array[String]) = {
+    println(Stream(1,2))
+    println(Stream(1,2,3,4,5).take(2))
+    println(Stream(1,2,3,4,5).drop(2))
+    println(Stream(1,2,3,4,5).take(2).toList)
+    println(Stream(1,2,3,4,5).drop(2).toList)
+  }
+}
