1e96d16a01e9f2c39ec58a9374d91862f9d81a92
shigemk2 <i.am.shige@gmail.com>
2015-06-13 20:42:07 +0900

    5-11 unfold

---
+++ chapter05/11.answer.scala
@@ -0,0 +1,47 @@
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+    f(z) match {
+      case Some((h,s)) => cons(h, unfold(s)(f))
+      case None => empty
+    }
+}
+
+import Stream._
+trait Stream[+A] {
+  def toList: List[A] = {
+    @annotation.tailrec
+    def go(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Cons(h,t) => go(t(), h() :: acc)
+      case _ => acc
+    }
+    go(this, List()).reverse
+  }
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
+}
+
+object Answer11 {
+  import Stream._
+  def main(args: Array[String]) = {
+    println(Stream.unfold(0)(x => Option(x, (x + 1) * 2)).take(10).toList) // List(0, 2, 6, 14, 30, 62, 126, 254, 510, 1022)
+  }
+}
