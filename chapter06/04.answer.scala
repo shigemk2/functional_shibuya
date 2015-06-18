3be94982fbafa219a6c4e8c1b0f19c82c9f44713
shigemk2 <i.am.shige@gmail.com>
2015-06-18 21:18:00 +0900

    delete

--- chapter06/04.answer.scala
+++ chapter06/04.answer.scala
@@ -12,37 +12,6 @@ object RNG {
     }
   }
 
-  // def nonNegativeInt(rng: RNG): (Int, RNG) = {
-  //   val (i, r) = rng.nextInt
-  //   (if (i < 0) -(i + 1) else i, r)
-  // }
-
-  // def double(rng: RNG): (Double, RNG) = {
-  //   val (i, r) = nonNegativeInt(rng)
-  //   (i / (Int.MaxValue.toDouble + 1), r)
-  // }
-
-  // def boolean(rng: RNG): (Boolean, RNG) =
-  //   rng.nextInt match { case (i,rng2) => (i%2==0,rng2) }
-
-  // def intDouble(rng: RNG): ((Int, Double), RNG) = {
-  //   val (i, r1) = rng.nextInt
-  //   val (d, r2) = double(r1)
-  //   ((i, d), r2)
-  // }
-
-  // def doubleInt(rng: RNG): ((Double, Int), RNG) = {
-  //   val ((i, d), r) = intDouble(rng)
-  //   ((d, i), r)
-  // }
-
-  // def double3(rng: RNG): ((Double, Double, Double), RNG) = {
-  //   val (d1, r1) = double(rng)
-  //   val (d2, r2) = double(r1)
-  //   val (d3, r3) = double(r2)
-  //   ((d1, d2, d3), r3)
-  // }
-
   def ints(count: Int)(rng: RNG): (List[Int], RNG) =
     if (count == 0)
       (List(), rng)
