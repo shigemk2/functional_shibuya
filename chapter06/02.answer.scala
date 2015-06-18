76c0fbfee109c0d34f334d5c8387497228657148
shigemk2 <i.am.shige@gmail.com>
2015-06-18 20:49:03 +0900

    6-2 double

---
+++ chapter06/02.answer.scala
@@ -0,0 +1,42 @@
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+object RNG {
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = Simple(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
+
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
+  }
+}
+
+import State._
+
+case class State[S, +A](run: S => (A, S)) {
+}
+
+object State {
+  type Rand[A] = State[RNG, A]
+
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+}
+
+println(RNG.Simple(1))
+println(RNG.Simple(1).nextInt)
+println(RNG.Simple(-1).nextInt)
+println(RNG.double(RNG.Simple(10)))
+println(RNG.double(RNG.Simple(10)))
