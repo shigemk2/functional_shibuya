57ca1bd3f68607d50bcd834556a9f74cd02c623a
shigemk2 <i.am.shige@gmail.com>
2015-06-19 13:41:54 +0900

    6-5

---
+++ chapter06/05.answer.scala
@@ -0,0 +1,48 @@
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+object RNG {
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = Simple(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+
+  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  val _double: Rand[Double] =
+    map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
+}
+
+import State._
+
+case class State[S, +A](run: S => (A, S)) {
+}
+
+object State {
+  type Rand[A] = State[RNG, A]
+
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+}
+
+println(RNG.Simple(1))
+println(RNG._double)
