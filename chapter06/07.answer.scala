95ccac8115231e0f4756d77f8d1eb23677f9baf0
shigemk2 <i.am.shige@gmail.com>
2015-06-20 17:32:12 +0900

    6-7 できてない

---
+++ chapter06/07.answer.scala
@@ -0,0 +1,50 @@
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+object RNG {
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = Simple(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, r1) = ra(rng)
+      val (b, r2) = rb(r1)
+      (f(a, b), r2)
+    }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+}
+
+import State._
+
+case class State[S, +A](run: S => (A, S)) {
+}
+
+object State {
+  type Rand[A] = State[RNG, A]
+
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+}
+
+val a = RNG
+// println(a.map2(a.nonNegativeInt, a.nonNegativeInt)((x, y) => x + y).apply(RNG.Simple(-10)))
+println(a.sequence(List(RNG.Simple(-10), List(RNG.Simple(-10)))))
